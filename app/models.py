d5e1fc66e5a716949777f0781a4445d19c688b9d
tamagochka <tamagochka@gmail.com>
2024-05-14 17:00:18 +0300

    eightth chapter: changes in user model for followers support

--- app/models.py
+++ app/models.py
@@ -1,12 +1,21 @@
 from datetime import datetime, timezone
 from typing import Optional
-from sqlalchemy import String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, WriteOnlyMapped, relationship
+from sqlalchemy import String, ForeignKey, Table, Column, Integer, func, select, or_
+from sqlalchemy.orm import Mapped, mapped_column, WriteOnlyMapped, relationship, aliased
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 from hashlib import md5
+
+
+followers = Table(
+    'followers',
+    db.metadata,
+    Column('follower_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('followed_id', Integer, ForeignKey('user.id'), primary_key=True)
+)
+
 
 # загрузчик пользователя проверяет наличие пользователя с указанным id в БД для обеспечения работы flask-login
 @login.user_loader
@@ -26,6 +35,18 @@ class User(UserMixin, db.Model):
     posts: WriteOnlyMapped['Post'] = relationship(back_populates='author')
     about_me: Mapped[Optional[str]] = mapped_column(String(140))
     last_seen: Mapped[Optional[datetime]] = mapped_column(default=lambda: datetime.now(timezone.utc))
+    following: WriteOnlyMapped['User'] = relationship(  # все пользователи, на которых подписан пользователь
+        secondary=followers,  # таблица ассоциаций, используемая для связи пользователей и подписчиков
+        primaryjoin=(followers.c.follower_id == id),  # условие связи объекта с таблицей ассоциаций
+        secondaryjoin=(followers.c.followed_id == id),  # усл. связи объекта с таблицей ассоциаций на др. стороне связи
+        back_populates='followers'
+    )
+    followers: WriteOnlyMapped['User'] = relationship(  # все пользователи, которые подписаны на данного
+        secondary=followers,
+        primaryjoin=(followers.c.followed_id == id),
+        secondaryjoin=(followers.c.follower_id == id),
+        back_populates='following'
+    )
 
     def __repr__(self):  # метод класса, определяющий каким образом отображать значения экземпляров класса при их выводе
         return f'<User {self.username}>'
@@ -42,6 +63,53 @@ class User(UserMixin, db.Model):
         # d=identicon - какое изображение предоставлять пользователям у которых нет аватара в сервисе
         # s={size} - размер предоставляемого аватара
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.following.add(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.following.remove(user)
+
+    def is_following(self, user):
+        query = self.following.select().where(User.id == user.id)
+        return db.session.scalar(query) is not None
+
+    def followers_count(self):
+        query = select(func.count()).select_from(self.followers.select().subquery())
+        return db.session.scalar(query)
+
+    def following_count(self):
+        query = select(func.count()).select_from(self.following.select().subquery())
+        return db.session.scalar(query)
+
+    def following_posts(self):
+        # использование алисов обязательно, чтобы была возможность различать о ком идет речь в запросе
+        Author = aliased(User)
+        Follower = aliased(User)
+        return (
+            select(Post)  # выбрать из таблицы Post
+            # все поля и присоединить к ней поля из таблицы Author(User), связанные с Post.author (авторы постов)
+            .join(Post.author.of_type(Author))  # присоединить к инф. о постах инф. об авторе поста
+            # выбрать из полученной таблицы все поля
+            # и присоединить к ней поля из таблицы Follower(User), связанные с Author.followers (подписчики автора)
+            # получится таблица, содержащая всех подписчиков и посты, которые они должны получить
+            # post.id post.text post.user_id == author.id author.username follower.id follower.username
+            .join(Author.followers.of_type(Follower), isouter=True)
+            # isouter - не отбрасывать поля с левой стороны, у которых нет соответствия с правой
+            # будут получены и посты тех авторов, у которых нед подписоты
+            # слева таблица Post+Author справа Follower
+            # если не указать isouter, то посты авторов, у которых нет подписчиков будут отброшены,
+            # будут только те поля у которых есть соответствие и справа и слева
+            # выбрать из полученной таблицы поля для текущего пользователя
+            .where(or_(  # т.е. посты которые он должен увидеть
+                Follower.id == self.id,  # посты авторов, на которые он подписан
+                Author.id == self.id  # посты автором которых он является
+            ))
+            .group_by(Post)
+            .order_by(Post.timestamp.desc())  # сортировать посты по времени их добавления по полю timestamp
+        )
 
 
 class Post(db.Model):
